2c4c8bac3d2519911d4a05cb50a65f71e4c9f274
Avital <avitals5050@gmail.com>
2024-09-19 16:14:05 +0300

    Initial commit - setup React project

---
+++ src/jsx/App.jsx
@@ -0,0 +1,18 @@
+
+import { Outlet, Route, Routes } from 'react-router-dom';
+import '../css/App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      {/* <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path='*' element={<Login />} />
+      </Routes> */}
+      hi there
+    </>
+  );
+}
+
+export default App;
